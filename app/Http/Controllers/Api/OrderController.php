0c1079ed6e151f77d86fd828ebd48088a4af4d9b
lhex22 <lhexcabanto18@gmail.com>
2022-05-23 20:18:21 +0800

    API order and order_product controller

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Payment;
+use App\Models\Product;
+use App\Models\Cart;
 use App\Models\Customer;
 use App\Models\Employee;
+use App\Models\OrderProduct;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,7 +22,73 @@ class OrderController extends Controller
      */
     public function index()
     {
-      
+        // $user = Auth::user();
+        // $order = Order::where('employee_id', $user->id)->with(['payments', 'customers', 'employees'])->get();
+
+        // return response($order,200);
+    }
+    public function confirmedOrder($id) //Confirmed Order in the Cart
+    {
+        $user = Auth::user();
+        $emp = $user->id;
+        $customer = Customer::find($id);
+        $cart = Cart::where('customer_id', $id)->where('status', 0)->get();
+        $cusTotal = $cart->sum('total');
+        $qtyCount = $cart->sum('quantity');
+        
+        $order = Order::create([
+            'total' => $cusTotal,
+            'order_qty' => $qtyCount,
+            'customer_id' => $id,
+            'employee_id' => $emp,
+        ]);
+        foreach ($cart as $c) {
+        $orderId = $order->id;
+        $order_product = OrderProduct::create([
+            'quantity' => $c->quantity,
+            'price' => $c->price,
+            'product_id' => $c->product_id,
+            'comment' => $c->comment,
+            'order_id' => $orderId,
+        ]);
+        $qty = $order_product->quantity;
+        $prod = $order_product->product_id;
+        $product = Product::find($prod);
+        $product->decrement('stock', $qty);
+        
+        $c->update([
+            'status'=> 1,
+        ]);
+    }
+
+        return response()->json('Confirmed Order');
+    }
+    public function payment(Request $request, $id) // Payment of the Order Product
+    {
+        $user = Auth::user();
+
+        $customer = Customer::with(['orders' =>function($qu) use($user){
+            return $qu->where('employee_id', $user->id)->whereHas('order_product', function($op){
+                return $op->where('status', 0);});
+        },'orders.order_product' =>function($que){
+            return $que->with(['products','options']);
+        }])->find($id);
+
+        $order = Order::where('customer_id', $id)->where('status', 0)->whereHas('order_product')->first();
+
+        $payment = new Payment();
+        $payment->paid = $request->paid;
+        $payment->change = $request->paid - $order->total;
+        $payment->save();
+        $paymentId = $payment->id;
+        $payment['total'] = $order->total;
+        
+        $order->payment_id = $paymentId;
+        $order->save();
+        $order->update(['status' => 1,]);
+        // $order->order_product()->update(['status' => 1]);
+
+        return response($payment,200);
     }
 
     /**
@@ -39,11 +108,12 @@ class OrderController extends Controller
      * @param  \App\Models\Order  $order
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Order $order, $id)
     {
-        $user = Auth::user();
-        $order = Order::where('customer_id', $id)->with(['payments', 'customers', 'employees'])->get();
-        return response($order,200);
+        // $user = Auth::user();
+        // $order = Order::where('customer_id', $id)->with(['payments', 'customers', 'employees'])->get();
+ 
+        // return response($order,200);
     }
 
     /**
@@ -53,7 +123,7 @@ class OrderController extends Controller
      * @param  \App\Models\Order  $order
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Order $order)
     {
         //
     }
@@ -64,7 +134,7 @@ class OrderController extends Controller
      * @param  \App\Models\Order  $order
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Order $order)
     {
         //
     }
--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\OrderProductController;
 use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\ProfileController;
-use App\Http\Controllers\Api\CartController;
-use App\Http\Controllers\OtpController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,26 +30,33 @@ Route::post('/login',[AuthController::class,'login']);
 Route::group(['middleware'=>['auth:api']],function(){
     //Employee Profile
     Route::get('employeedetails',[ProfileController::class,'employeeDetails']);
+    Route::post('uploadImage',[ProfileController::class,'upImage']);
     Route::post('update-profile',[ProfileController::class,'updateProfile']);
     Route::post('changepassword',[ProfileController::class,'changePassword']);
     Route::post('logout',[AuthController::class,'logout']);
+    
+    //Forgot Password
+    Route::any('request_otp',[AuthController::class, 'requestOtp']);
+    Route::post('verify_otp',[AuthController::class, 'verifyOtp']);
+    Route::post('new_password',[AuthController::class, 'newPassword']);
 
     //Product
     Route::get('products',[ProductController::class, 'index']);
+    Route::get('categories',[ProductController::class, 'catDisplay']);
+    Route::get('products/outStock',[ProductController::class, 'outStock']);
     Route::get('product-show/{id}',[ProductController::class, 'show']);
 
     //Customer
     Route::get('customers',[CustomerController::class, 'index']);
+    // Route::get('customerorders',[CustomerController::class,'customerOrders']);
     Route::get('customer-show/{id}',[CustomerController::class, 'show']);
-    //insert customer w/ QR Code
     Route::post('customer/store',[CustomerController::class,'store']);
 
-    //Order
-    Route::get('orders',[OrderController::class,'index']);
-    Route::get('order-show/{id}',[OrderController::class,'show']);
-
-    //Payment
-    // Route::post('payment/{id}',[PaymentController::class,'store']);
+    //Order//
+    Route::post('confirmedorder/{id}',[OrderController::class,'confirmedOrder']);
+    Route::post('paymentorder/{id}',[OrderController::class,'payment']);
+    // Route::get('orders',[OrderController::class,'index']);
+    // Route::get('order-show/{id}',[OrderController::class,'show']);
 
     //Cart
     Route::get('cart',[CartController::class,'index']);
@@ -59,20 +65,18 @@ Route::group(['middleware'=>['auth:api']],function(){
     Route::post('addtocart',[CartController::class,'store']);
     Route::post('updatecart/{id}',[CartController::class,'update']);
 
-    //Order Product
-    Route::get('customerorders/{id}',[OrderProductController::class,'customerOrders']);
-    Route::get('deleteorder/{id}',[OrderProductController::class,'destroy']);
+    //Order Product//
+    // Route::get('customerorders/{id}',[OrderProductController::class,'customerOrders']);
+    // Route::get('deleteorder/{id}',[OrderProductController::class,'destroy']);
     Route::get('orderproduct',[OrderProductController::class,'index']);
-    Route::get('orderproduct/{id}',[OrderProductController::class,'show']);
+    Route::get('orderproduct/paid',[OrderProductController::class,'displayPaid']);
+    Route::get('orderproduct/pending',[OrderProductController::class,'displayPending']);
+    Route::get('orderDisplay/{id}',[OrderProductController::class,'displayP']);
+    // Route::get('orderproduct/{id}',[OrderProductController::class,'show']);
     Route::get('toprepared',[OrderProductController::class,'toPrepared']);
-    Route::post('confirmedorder/{id}',[OrderProductController::class,'confirmedOrder']);
+    // Route::post('confirmedorder/{id}',[OrderProductController::class,'confirmedOrder']);
     Route::post('markprepared/{id}',[OrderProductController::class,'markPrepared']);
-    Route::post('orderproduct/store',[OrderProductController::class,'store']);
-    Route::post('paymentorder/{id}',[OrderProductController::class,'payment']);
-    Route::post('updateorder/{id}',[OrderProductController::class,'update']);
-
-    //Forgot Password
-    Route::any('request_otp',[AuthController::class, 'requestOtp']);
-    Route::post('verify_otp',[AuthController::class, 'verifyOtp']);
-    Route::post('new_password',[AuthController::class, 'newPassword']);
+    // Route::post('orderproduct/store',[OrderProductController::class,'store']);
+    // Route::post('paymentorder/{id}',[OrderProductController::class,'payment']);
+    // Route::post('updateorder/{id}',[OrderProductController::class,'update']);
 });
