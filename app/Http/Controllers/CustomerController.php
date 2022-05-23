d39ac864ed4d59645806aa52cdf7b7a2cbd6a414
lhex22 <lhexcabanto18@gmail.com>
2022-05-23 17:38:08 +0800

    qrCode

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Customer;
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        // $customer = Customer::with('companies')->get();
+         $company = Company::where('id', $user->company_id)->with('customers')
+        ->get();
+
+        return response($company,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $customer = Customer::find($id);
+        $company = $customer->companies;
+        $company = $customer;
+        return $customer;
+    }
+
+    public function showOrders()
+    {
+        $user = Auth::user();
+        $company = Company::where('id', $user->company_id)->with('customers')
+        ->get();
+
+        return response($company,200);
+
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\ProductOrderController;
+use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\OrderProductController;
+use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\OtpController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,16 +27,51 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 */
-//adminCompany
-Route::post('/adminLogin',[CAAuthController::class,'adminLogin']);
-Route::group(['middleware'=>['auth:api']],function(){
-    Route::post('adminLogout',[CAAuthController::class,'adminLogout']);
-});
-
 Route::post('/login',[AuthController::class,'login']);
 Route::group(['middleware'=>['auth:api']],function(){
-    Route::post('productOrder',[ProductOrderController::class,'store']);
-    // Route::get('orders',[OrderController::class,'index']);
-    Route::get('orders/{id}',[OrderController::class,'show']);
+    //Employee Profile
+    Route::get('employeedetails',[ProfileController::class,'employeeDetails']);
+    Route::post('update-profile',[ProfileController::class,'updateProfile']);
+    Route::post('changepassword',[ProfileController::class,'changePassword']);
     Route::post('logout',[AuthController::class,'logout']);
+
+    //Product
+    Route::get('products',[ProductController::class, 'index']);
+    Route::get('product-show/{id}',[ProductController::class, 'show']);
+
+    //Customer
+    Route::get('customers',[CustomerController::class, 'index']);
+    Route::get('customer-show/{id}',[CustomerController::class, 'show']);
+    Route::post('customer/store',[CustomerController::class,'store']);
+
+    //Order
+    Route::get('orders',[OrderController::class,'index']);
+    Route::get('order-show/{id}',[OrderController::class,'show']);
+
+    //Payment
+    // Route::post('payment/{id}',[PaymentController::class,'store']);
+
+    //Cart
+    Route::get('cart',[CartController::class,'index']);
+    Route::get('cart/{id}',[CartController::class,'show']);
+    Route::get('deletecart/{id}',[CartController::class,'destroy']);
+    Route::post('addtocart',[CartController::class,'store']);
+    Route::post('updatecart/{id}',[CartController::class,'update']);
+
+    //Order Product
+    Route::get('customerorders/{id}',[OrderProductController::class,'customerOrders']);
+    Route::get('deleteorder/{id}',[OrderProductController::class,'destroy']);
+    Route::get('orderproduct',[OrderProductController::class,'index']);
+    Route::get('orderproduct/{id}',[OrderProductController::class,'show']);
+    Route::get('toprepared',[OrderProductController::class,'toPrepared']);
+    Route::post('confirmedorder/{id}',[OrderProductController::class,'confirmedOrder']);
+    Route::post('markprepared/{id}',[OrderProductController::class,'markPrepared']);
+    Route::post('orderproduct/store',[OrderProductController::class,'store']);
+    Route::post('paymentorder/{id}',[OrderProductController::class,'payment']);
+    Route::post('updateorder/{id}',[OrderProductController::class,'update']);
+
+    //Forgot Password
+    Route::any('request_otp',[AuthController::class, 'requestOtp']);
+    Route::post('verify_otp',[AuthController::class, 'verifyOtp']);
+    Route::post('new_password',[AuthController::class, 'newPassword']);
 });
