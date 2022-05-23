dacff381c4476b43f6176427395572195b38a020
lhex22 <lhexcabanto18@gmail.com>
2022-05-23 18:30:04 +0800

    QrCode

---
+++ app/Http/Controllers/Api/CustomerController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Customer;
+// use App\Models\Company;
+use App\Models\Order;
+use BaconQrCode\Common\ErrorCorrectionLevel;
+use Illuminate\Http\Request;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Illuminate\Support\Facades\Storage;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $user = Auth::user();
+        //  $company = Company::where('id', $user->company_id)->with('customers')
+        // ->first();
+
+        // return response($company,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+        $customer = new Customer();
+        $customer->name = $request->name;
+        $customer->phone = $request->phone;
+        $customer->email = $request->email;
+        $customer->qrcode = 'UB-'.substr(str_shuffle("0123456789"),0,7);
+        // QrCode::size(400)->format('png')->generate($customer,'../storage/app/public/qrimages/' . $cust);
+        // $customers = Customer::where('id', $customer->id)->get();
+        // $customer->companies()->attach($user->company_id);
+        if(!Storage::exists(storage_path('app/public/qrimages/'.$customer->qrcode.'.png'))){
+            QrCode::size(400)->format('png')->generate($customer->qrcode, storage_path('app/public/qrimages/'.$customer->qrcode.'.png'));
+        }
+        $customer->qrimage ='../storage/app/public/qrimages/'.$customer->qrcode. '.png';
+        $customer->save();
+        return response()->json($customer);
+       
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = Auth::user();
+        return Customer::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //4
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
